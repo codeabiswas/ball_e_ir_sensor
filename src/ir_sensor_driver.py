d3450d10b60153a2e4eb2568e328a17873b16802
Andrei Biswas <axb6972@rit.edu>
2021-05-04 17:19:15 -0400

    Updated Docs

--- src/ir_sensor_driver.py
+++ src/ir_sensor_driver.py
@@ -1,24 +1,44 @@
+"""
+ir_sensor_driver.py
+---
+This file contains the main method to get information from the IR sensor.
+NOTE: This is not being used and has not been developed much because the sensors themselves do not work very well and given how the mechanical build is for the feed and how the entire system has been programmed, this sensor is not needed.
+---
+
+Author: Andrei Biswas (@codeabiswas)
+Date: May 4, 2021
+Last Modified: May 04, 2021
+"""
+
 import time
 
 import Jetson.GPIO as gpio
 
-gpio.setmode(gpio.BOARD)
+def main():
+    """Main prototype/testing area. Code prototyping and checking happens here."""
 
-sensor_output = 35
+    gpio.setmode(gpio.BOARD)
 
-gpio.setup(sensor_output, gpio.IN)
+    # Set the sensor output pin
+    sensor_output = 35
+    gpio.setup(sensor_output, gpio.IN)
 
-counter = 1
+    # Counter for how many times an object is detected
+    counter = 1
 
-try:
-    while True:
-        if gpio.input(sensor_output) == 1:
-            print('{}: Found something!'.format(counter))
-            time.sleep(1)
-            counter+=1
-        # print(gpio.input(sensor_output))
-        # time.sleep(1)
-finally:
-    gpio.cleanup()
-    print('cleaned up')
+    # Loop until the user exits the loop manually using <C(trl)-c>
+    try:
+        while True:
+            if gpio.input(sensor_output) == 1:
+                print('{}: Found Something!'.format(counter))
+                time.sleep(1)
+                counter+=1
+    # If loop is exited, clean the pins for future use
+    finally:
+        gpio.cleanup()
+        print('Cleaned Up')
+
+if __name__ == "__main__":
+    # Run the main function
+    main()
 
